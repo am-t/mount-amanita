30b44cda1b4f052adf656b72393c4489bf584349
Amanda Tom <amanda.my.tom@gmail.com>
2015-06-29 15:32:09 -0400

    StartScreen

---
+++ Assets/Scripts/StartScreen/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Canvas creditsMenu;
+    public Button startButton;
+    public Button creditsButton;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    creditsMenu = creditsMenu.GetComponent<Canvas>();
+	    startButton = startButton.GetComponent<Button>();
+	    creditsButton = creditsButton.GetComponent<Button>();
+	    creditsMenu.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
