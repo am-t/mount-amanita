328b383277cf252b629f6a295e2f7d778252c4cd
Amanda Tom <amanda.my.tom@gmail.com>
2015-07-02 09:35:16 -0400

    EffectInitialize push

---
+++ Assets/Scripts/AssignEffect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssignEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //All scripts: disabled
+    //Put all the effects in an array, mix the contents of the arrray
+    //Muse[1] -> array[1] [...]
+    //
+}
---
+++ Assets/Scripts/EffectInitialize.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectInitialize : MonoBehaviour
+{
+
+    public GameController gc;
+    public GameObject[] effectArray = new GameObject[4];
+    private int currentIndex = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    gc = gameObject.GetComponent<GameController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void startEffects(float difficulty)
+    {
+        int newIndex = Random.Range(0, effectArray.Length);
+        
+        //Get currently active muses
+        //Make an array of effects
+        for (int i = 0; i < effectArray.Length; i++) {
+            if (gc.museStatus[i] == 1) { 
+                effectArray[currentIndex].SetActive(false);
+                currentIndex = newIndex;
+                effectArray[currentIndex].SetActive(true);
+            }
+        }
+
+
+    }
+}
